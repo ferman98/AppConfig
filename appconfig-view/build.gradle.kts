5bf9408701ea1be9e75df65c2ba563e8e784c369
ferman2 <cewekece2016@gmail.com>
2024-03-11 21:55:01 +0700

    [FEAT] enhance gradle

--- appconfig-view/build.gradle.kts
+++ appconfig-view/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    `maven-publish`
     alias(libs.plugins.gradlePlugin.library)
     alias(libs.plugins.gradlePlugin.kotlin)
 }
@@ -23,36 +22,6 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-}
-
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "io.github.ferman98.appconfig"
-            artifactId = "view"
-            version = "1.0.0"
-
-            afterEvaluate {
-                from(components["release"])
-            }
-        }
-    }
-    repositories {
-        maven {
-            name = "myrepo"
-            url = uri("${project.layout.buildDirectory}/repo")
-        }
-    }
-}
-
-tasks.register<Zip>("generateRepo") {
-    val publishTask = tasks.named(
-        "publishReleasePublicationToMyrepoRepository",
-        PublishToMavenRepository::class.java
-    )
-    from(publishTask.map { it.repository.url })
-    into("mylibrary")
-    archiveFileName.set("mylibrary.zip")
 }
 
 dependencies {
